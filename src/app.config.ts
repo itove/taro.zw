1059b0da90462b557b8a7f318e143ddb592a47de
Al Zee <z@alz.ee>
2024-05-27 09:22:39 +0800

    tabbar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,46 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/leyou/index',
+    'pages/nav/index',
+    'pages/me/index',
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    // custom: true,
+    color: '#808080',
+    selectedColor: '#0FB99F',
+    backgroundColor: '#FFFFFF',
+    list: [
+      {
+        "pagePath": "pages/index/index",
+        "text": "走进东沟",
+        "iconPath": './icons/hill.png',
+        "selectedIconPath": './icons/hill-fill.png'
+      },
+      {
+        "pagePath": "pages/leyou/index",
+        "text": "乐游东沟",
+        "iconPath": './icons/hill-river.png',
+        "selectedIconPath": './icons/hill-river-fill.png'
+      },
+      {
+        "pagePath": "pages/nav/index",
+        "text": "智慧导览",
+        "iconPath": './icons/pavilion.png',
+        "selectedIconPath": './icons/pavilion-fill.png'
+      },
+      {
+        "pagePath": "pages/me/index",
+        "text": "个人中心",
+        "iconPath": './icons/user.png',
+        "selectedIconPath": './icons/user-fill.png'
+      },
+    ]
   }
 })
