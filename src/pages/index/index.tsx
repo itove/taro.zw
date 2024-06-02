dd13e880165a9b7593e56780aa1f9cc522461566
Al Zee <z@alz.ee>
2024-06-02 17:03:11 +0800

    index done

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Image } from '@tarojs/components'
 import { Button } from "@nutui/nutui-react-taro"
 import './index.scss'
-import { Grid, NoticeBar, Swiper } from '@nutui/nutui-react-taro'
+import { Grid, NoticeBar, Swiper, Tabs } from '@nutui/nutui-react-taro'
 import Hill from '../../icons/hill-river-fill.png'
 import Slider1 from '../../images/slider1.png'
 import Speaker from '../../icons/speaker.png'
 import HillRiver from '../../icons/hill-river-1.png'
 import Daolan from '../../images/zhihuidaolan.png'
-
+import Image1 from '../../images/image1.png'
 
 const grid = []
 const items = [
@@ -109,6 +109,66 @@ const noticeBar =
       />
 
 function Index() {
+  const [tab1value, setTab1value] = useState<string | number>('0')
+
+  const tabPaneList = [
+    {
+      p: Image1,
+      t: '云上牡丹园',
+      s: '东东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
+    },
+    {
+      p: Image1,
+      t: '云上牡丹园',
+      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
+    },
+    {
+      p: Image1,
+      t: '云上牡丹园',
+      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
+    },
+    {
+      p: Image1,
+      t: '云上牡丹园',
+      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
+    },
+    {
+      p: Image1,
+      t: '云上牡丹园',
+      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
+    },
+  ]
+  const tabPane1 = []
+
+  tabPaneList.map((i, index) => {
+    tabPane1.push(
+      <View key={index} className="tab-item">
+        <View className="img">
+          <Image className="w-100 rounded" src={i.p} mode="widthFix" />
+        </View>
+        <View className="text">
+        {i.t}
+        <p>{i.s}</p>
+        </View>
+      </View>
+    )
+  })
+
+  const tabs = 
+    <Tabs
+      value={tab1value}
+      onChange={(value) => {
+        setTab1value(value)
+      }}
+      activeType="button"
+      >
+      <Tabs.TabPane title="游在东沟"> {tabPane1} </Tabs.TabPane>
+      <Tabs.TabPane title="住在东沟"> 住在东沟 </Tabs.TabPane>
+      <Tabs.TabPane title="吃在东沟"> 吃在东沟 </Tabs.TabPane>
+      <Tabs.TabPane title="购在东沟"> 购在东沟 </Tabs.TabPane>
+    </Tabs>
+
+
   return (
     <View className="p-1 home">
       <View className="hell">
@@ -128,7 +188,7 @@ function Index() {
             src={HillRiver}
           />
         </View>
-        <Image className="w-100" src={Daolan} mode="widthFix" />
+        <Image className="w-100 rounded" src={Daolan} mode="widthFix" />
       </View>
 
       <View className="zoujin block">
@@ -142,7 +202,7 @@ function Index() {
         </View>
         <View className="wrapper">
           <View className="">
-            <Image className="w-100" src={Daolan} mode="center" />
+            <Image className="w-100 rounded" src={Daolan} mode="center" />
             <View class="text">
               东沟简介
               <p>点击查看></p>
@@ -150,14 +210,14 @@ function Index() {
           </View>
           <View className="col2">
             <View className="">
-              <Image className="w-100" src={Daolan} mode="center" />
+              <Image className="w-100 rounded" src={Daolan} mode="center" />
               <View class="text">
                 东沟文化
                 <p>爱国主义教育基地</p>
               </View>
             </View>
             <View className="">
-              <Image className="w-100" src={Daolan} mode="center" />
+              <Image className="w-100 rounded" src={Daolan} mode="center" />
               <View class="text">
                 东沟历史
                 <p>瞻仰革命圣迹</p>
@@ -176,7 +236,7 @@ function Index() {
             src={HillRiver}
           />
         </View>
-        <Image className="w-100" src={Daolan} mode="widthFix" />
+        {tabs}
       </View>
 
     </View>
