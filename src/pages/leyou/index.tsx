eea31b7fc23299732075ad9da56de1b6bfad55c7
Al Zee <z@alz.ee>
2024-06-07 00:40:15 +0800

    nth

--- src/pages/leyou/index.tsx
+++ src/pages/leyou/index.tsx
@@ -16,6 +16,47 @@ function scrollTo() {
   })
 }
 
+function SwiperItem({node, index}) {
+  return (
+    <Swiper.Item className="slide-item">
+    <Image
+    className="w-100 img"
+    mode="widthFix"
+    onClick={() => console.log(index)}
+    src={Env.imageUrl + node.image}
+    alt=""
+    />
+    <View className="text">
+    <p className="title">{node.title}</p>
+    <p className="summary ellipsis-2">{node.summary}</p>
+    </View>
+    </Swiper.Item>
+  )
+}
+
+function ViewItem({node, index}) {
+  return (
+    <View key={index} className="list-item">
+      <View className="img">
+        <Image className="w-100 rounded" src={node.image} mode="widthFix" />
+      </View>
+      <View className="text">
+      <View>{node.title}<span class="d-inline-block badge ms-1">民宿</span></View>
+      <p className="ellipsis-2 my-1">{node.summary}</p>
+      <p class="call"><img width="17px" height="17px" src={Call} /> 电话：0719-8888888</p>
+      </View>
+    </View>
+  )
+}
+
+function GridItem({node, index}) {
+  return (
+    <Grid.Item text={node.title} key={index} className="grid-list rounded overflow-hidden">
+      <Image className="w-100" src={node.image} mode="widthFix" />
+    </Grid.Item>
+  )
+}
+
 function Index() {
   const [youList, setYouList] = useState([])
   const [zhuList, setZhuList] = useState([])
@@ -30,130 +71,15 @@ function Index() {
       const data = res.data
       console.log(res)
 
-      setSliderList(data.slider.map((node, index) => <SwiperItem node={node} index={index} />))
-      setYouList(data.youzai.map((node, index) => <TabPane node={node} index={index} />))
-      setZhuList(data.zhuzai.map((node, index) => <TabPane node={node} index={index} />))
-      setChiList(data.chizai.map((node, index) => <TabPane node={node} index={index} />))
-      setGouList(data.gouzai.map((node, index) => <TabPane node={node} index={index} />))
-      setGridList(gridItems.map((node, index) => <GridItem node={node} index={index} />))
-      setDili(data.dili[0])
-      setZoujin(data.zoujin[0])
-      setWenhua(data.wenhua[0])
-      setLishi(data.lishi[0])
+      setYouList(data.youzai.map((node, index) => <SwiperItem node={node} index={index} />))
+      setZhuList(data.zhuzai.map((node, index) => <ViewItem node={node} index={index} />))
+      setChiList(data.chizai.map((node, index) => index < 4 && <GridItem node={node} index={index} />))
+      setGouList(data.gouzai.map((node, index) => <SwiperItem node={node} index={index} />))
     })
     .catch(err => {
       console.log(err)
     })
   }, [])
-
-  const swiperList = [
-    {
-      p: Slider1,
-      link: '',
-      t: '云上牡丹园',
-      s: '东沟·云云上牡丹园地处于十堰云上牡丹园地处于十堰云上牡丹园地处于十堰云上牡丹园地处于十堰云上牡丹园地处于十堰云上牡丹园地处于十堰上牡丹园地处于十堰市茅箭区茅塔乡...',
-    },
-    {
-      p: Slider1,
-      link: '',
-      t: '云上牡丹园',
-      s: '东沟·云上牡丹园地处于十堰',
-    },
-    {
-      p: Slider1,
-      link: '',
-      t: '云上牡丹园',
-      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡...',
-    },
-  ]
-
-  const swiper =  
-        {swiperList.map((item, index) => (
-          <Swiper.Item key={item} className="slide-item">
-            <Image
-              className="w-100 img"
-              mode="widthFix"
-              onClick={() => console.log(index)}
-              src={item.p}
-              alt=""
-            />
-            <View className="text">
-            <p className="title">{item.t}</p>
-            <p className="summary ellipsis">{item.s}</p>
-            </View>
-          </Swiper.Item>
-        ))}
-
-  const zhuzaiList = [
-    {
-      p: Image1,
-      t: '云上牡丹园',
-      s: '东东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
-    },
-    {
-      p: Image1,
-      t: '云上牡丹园',
-      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
-    },
-    {
-      p: Image1,
-      t: '云上牡丹园',
-      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
-    },
-    {
-      p: Image1,
-      t: '云上牡丹园',
-      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
-    },
-    {
-      p: Image1,
-      t: '云上牡丹园',
-      s: '东沟·云上牡丹园地处于十堰市茅箭区茅塔乡东沟村二组，平均海拔...',
-    },
-  ]
-  const zhuzai = []
-
-  zhuzaiList.map((i, index) => {
-    zhuzai.push(
-      <View key={index} className="list-item">
-        <View className="img">
-          <Image className="w-100 rounded" src={i.p} mode="widthFix" />
-        </View>
-        <View className="text">
-        <View>{i.t}<span class="d-inline-block badge ms-1">民宿</span></View>
-        <p className="ellipsis my-1">{i.s}</p>
-        <p class="call"><img width="17px" height="17px" src={Call} /> 电话：0719-8888888</p>
-        </View>
-      </View>
-    )
-  })
-
-  const grid = []
-  const gridList = [
-    {
-      t: '茅塔百花蜜',
-      p: Image1 
-    },
-    {
-      t: '住在东沟',
-      p: Image1
-    },
-    {
-      t: '吃在东沟',
-      p: Image1
-    },
-    {
-      t: '购在东沟',
-      p: Image1
-    },
-  ]
-  gridList.map((i, index) => {
-    grid.push(
-      <Grid.Item text={i.t} key={index} className="grid-list rounded overflow-hidden">
-        <Image className="w-100" src={i.p} mode="widthFix" />
-      </Grid.Item>
-    )
-  })
 
   return (
     <View className="leyou p-1">
@@ -165,7 +91,7 @@ function Index() {
           游在东沟
         </View>
         <Swiper defaultValue={0} loop className="slide" height="230">
-          {swiper}
+          {youList}
         </Swiper>
       </View>
 
@@ -177,7 +103,7 @@ function Index() {
           住在东沟
         </View>
         <View class="list p-1 rounded">
-        {zhuzai}
+        {zhuList}
         </View>
       </View>
 
@@ -190,7 +116,7 @@ function Index() {
           <View class="more"> 更多 > </View>
         </View>
         <Grid columns="2" gap="3" center={false} className="">
-          {grid}
+          {chiList}
         </Grid>
       </View>
 
@@ -203,7 +129,7 @@ function Index() {
           <View class="more"> 更多 > </View>
         </View>
         <Swiper defaultValue={0} loop className="slide" height="230">
-          {swiper}
+          {gouList}
         </Swiper>
       </View>
 
