1fe9788f94cc9692f3b12bcbcc41b4c250c9d378
Al Zee <z@alz.ee>
2024-06-07 03:07:11 +0800

    nth

--- src/pages/node/index.tsx
+++ src/pages/node/index.tsx
@@ -1,48 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import Taro from '@tarojs/taro'
+import { Env } from '../../env'
 import { View, Image } from '@tarojs/components'
 import { Grid } from '@nutui/nutui-react-taro'
 import './index.scss'
 import Image1 from '../../images/image1.png'
 
 function Index() {
+  const instance = Taro.getCurrentInstance();
+  const region = instance.router.params.region
+
+  const [list, setList] = useState([])
+
   useEffect(() => {
     Taro.setNavigationBarTitle({
       title: '吃在东沟'
     })
-  }, [])
+    Taro.request({
+      url: Env.apiUrl + 'nodes/' + region
+    })
+    .then(res => {
+      const data = res.data
+      console.log(res)
 
-  const grid = []
-  const gridList = [
-    {
-      t: '茅塔百花蜜',
-      p: Image1 
-    },
-    {
-      t: '住在东沟',
-      p: Image1
-    },
-    {
-      t: '吃在东沟',
-      p: Image1
-    },
-    {
-      t: '购在东沟',
-      p: Image1
-    },
-  ]
-  gridList.map((i, index) => {
-    grid.push(
-      <Grid.Item text={i.t} key={index} className="grid-list rounded overflow-hidden">
-        <Image className="w-100" src={i.p} mode="widthFix" />
-      </Grid.Item>
-    )
-  })
+      setList(data.map((node, index) =>
+          <Grid.Item text={node.title} key={index} className="grid-list rounded overflow-hidden" onClick={() => gotoNode(node.id)}>
+            <Image className="w-100" src={Env.imageUrl + node.image} mode="widthFix" />
+          </Grid.Item>
+        )
+      )
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
 
   return (
     <View className="node-index p-1">
       <Grid columns="2" gap="3" center={false} className="">
-        {grid}
+        {list}
       </Grid>
     </View>
   )
