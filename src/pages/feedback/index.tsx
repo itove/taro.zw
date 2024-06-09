1774280af5de238746e79e560911e78326b6520e
Al Zee <z@alz.ee>
2024-06-09 15:21:43 +0800

    nth

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, Button } from '@tarojs/components'
 import './index.scss'
+import Taro from '@tarojs/taro'
+import { Env } from '../../env'
 import {
   Form,
   InputNumber,
@@ -10,7 +12,30 @@ import {
 
 function Index() {
 
-  const formSubmit = () => {
+  const formSubmit = (data) => {
+    console.log(data);
+    Taro.request({
+      method: 'POST',
+      url: Env.apiUrl + 'feedback',
+      data
+    }).then((res) => {
+      if (res.statusCode === 200) {
+        Taro.showToast({
+          title: '提交成功',
+          icon: 'success',
+          duration: 2000
+        }).then(() => {
+          Taro.reLaunch({ url: '/pages/index/index' })
+        })
+      } else {
+        Taro.showToast({
+          title: '系统错误',
+          icon: 'error',
+          duration: 2000
+        })
+        console.log('server error！' + res.errMsg)
+      }
+    })
   }
 
   const formReset = () => {
@@ -18,15 +43,19 @@ function Index() {
 
   return (
     <View className="p-1">
-      <Form onSubmit={formSubmit} onReset={formReset} >
+      <Form 
+        labelPosition="left"
+        onFinish={(values) => formSubmit(values)}
+        onFinishFailed={(values) => onFinishFailed(values)}
+      >
         <View className='example-body'>
         <Form.Item
           required
           label="姓名"
-          name="username"
+          name="firstname"
           rules={[
-            { max: 5, message: '字段A不能超过5个字' },
-            { required: true, message: '请输入字段A' },
+            { max: 50, message: '不能超过50个字' },
+            { required: true, message: '请输入姓名' },
           ]}
         >
           <Input type='text' placeholder='' focus/>
@@ -44,8 +73,8 @@ function Index() {
           label="电话"
           name="phone"
           rules={[
-            { max: 5, message: '字段A不能超过5个字' },
-            { required: true, message: '请输入字段A' },
+            { max: 11, message: '不能超过11个字' },
+            { required: true, message: '请输入电话' },
           ]}
         >
           <Input type='text' placeholder='' focus/>
@@ -56,8 +85,8 @@ function Index() {
           label="标题"
           name="title"
           rules={[
-            { max: 5, message: '字段A不能超过5个字' },
-            { required: true, message: '请输入字段A' },
+            { max: 50, message: '不能超过50个字' },
+            { required: true, message: '请输入标题' },
           ]}
         >
           <Input type='text' placeholder='' focus/>
@@ -67,8 +96,8 @@ function Index() {
           required
           name="body"
           rules={[
-            { max: 5, message: '字段A不能超过5个字' },
-            { required: true, message: '请输入字段A' },
+            { max: 500, message: '不能超过500个字' },
+            { required: true, message: '请输入内容' },
           ]}
         >
           <TextArea
@@ -81,7 +110,7 @@ function Index() {
           />
         </Form.Item>
         </View>
-        <Button className="btn-primary">提 交</Button>
+        <Button formType="submit" className="btn-primary">提 交</Button>
       </Form>
     </View>
   )
