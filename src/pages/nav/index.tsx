2b2b5d2515a0aed2be778178b1030c75c755849b
Al Zee <z@alz.ee>
2024-06-09 19:10:37 +0800

    nth

--- src/pages/nav/index.tsx
+++ src/pages/nav/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { View } from '@tarojs/components'
 import './index.scss'
-import Search from '../../icons/search.png'
-import Grid from '../../icons/grid.png'
+import Taro from '@tarojs/taro'
+import { Env } from '../../env'
 
 function Index() {
   return (
     <View className="">
       <View className="top">
         <View className="icon">
-          <img src={Search} />
+          <img src={Env.iconUrl + search.png} />
         </View>
         <View className="item active"> 景点 </View>
         <View className="item"> 卫生间 </View>
         <View className="item"> 出入口 </View>
         <View className="item"> 服务点 </View>
         <View className="icon">
-          <img src={Grid} />
+          <img src={Env.iconUrl + gird.png} />
         </View>
       </View>
     </View>
