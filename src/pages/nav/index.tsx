9045717e459135c7aa1bfa274078f4c24ef87b9d
Al Zee <z@alz.ee>
2024-06-14 03:59:16 +0800

    no more top menu

--- src/pages/nav/index.tsx
+++ src/pages/nav/index.tsx
@@ -139,19 +139,6 @@ function Index() {
 
   return (
     <View className="">
-      <View className="top">
-        <View className="icon">
-          <img src={Env.iconUrl + 'search.png'} />
-        </View>
-        <View className="item active"> 景点 </View>
-        <View className="item"> 卫生间 </View>
-        <View className="item"> 出入口 </View>
-        <View className="item"> 服务点 </View>
-        <View className="icon">
-          <img src={Env.iconUrl + 'gird.png'} />
-        </View>
-      </View>
-
       <Map
         id="map"
         className="map"
