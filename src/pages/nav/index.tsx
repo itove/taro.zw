c9533105399c36a765f9af7cad94d1f28ee91788
Al Zee <z@alz.ee>
2024-06-14 03:35:58 +0800

    works

--- src/pages/nav/index.tsx
+++ src/pages/nav/index.tsx
@@ -8,8 +8,6 @@ import { Popup } from '@nutui/nutui-react-taro'
 
 function Index() {
   const [showPop, setShowPop] = useState(false)
-  const url = 'https://map.vjingkeji.com/%E6%B1%9F%E8%A5%BF%E9%BE%99%E5%8D%97'
-  const url0 = Env.baseUrl + 'map'
 
   // const query = wx.createSelectorQuery()
   // const mapContext = Taro.createMapContext(query.select('.map'))
@@ -18,21 +16,24 @@ function Index() {
 
   const center = [32.499823, 110.83360]
 
-  const bounds0 = [32.500799, 110.83360]
-  const bounds1 = [32.499123, 110.83230]
+  const ne = [32.500799, 110.83360]
+  const sw = [32.499123, 110.83230]
+
+  const ne1 = [32.500799, 110.83621]
+  const sw1 = [32.499123, 110.83530]
 
   const markers = [
     {
       id: 0,
-      latitude: bounds0[0],
-      longitude: bounds0[1],
+      latitude: ne[0],
+      longitude: ne[1],
       width: 16,
       height: 24
     },
     {
       id: 0,
-      latitude: bounds1[0],
-      longitude: bounds1[1],
+      latitude: sw[0],
+      longitude: sw[1],
       width: 16,
       height: 24
     }
@@ -61,26 +62,26 @@ function Index() {
 
   const opacity = 1
   const zIndex = 1
-  const row = 4
-  const col = 4
-  const latPer = (bounds0[0] - bounds1[0]) / col
-  const longPer = (bounds0[1] - bounds1[1]) / row
+  const row = 2
+  const col = 1
+  const latPer = (ne[0] - sw[0]) / col
+  const longPer = (ne[1] - sw[1]) / row
 
   for (let i = 0; i < col; i++) {
-    const lat0 = bounds0[0] - latPer * i
-    const lat1 = bounds0[0] - latPer * (i + 1)
+    const lat0 = ne[0] - latPer * i
+    const lat1 = ne[0] - latPer * (i + 1)
     let long0
     let long1
     for (let j = 0; j < row; j++) {
-      long0 = bounds1[1] + longPer * (j + 1)
-      long1 = bounds1[1] + longPer * j
+      long0 = sw[1] + longPer * (j + 1)
+      long1 = sw[1] + longPer * j
       const northeast = {
         latitude: lat0,
         longitude: long0
       }
       const southwest = {
         latitude: lat1,
-        longitude: long0
+        longitude: long1
       }
       const bounds = {
         northeast,
@@ -88,8 +89,6 @@ function Index() {
       }
 
       const index = col * i + j
-      console.log(bounds)
-      console.log(index)
 
       const markers = [
         {
@@ -118,8 +117,8 @@ function Index() {
         console.log(err)
       })
 
-      // const src = Env.imageUrl + 'deb/o_' + index + '.jpg'
-      const src = Env.imageUrl + 'deb/deb.jpg'
+      const src = Env.imageUrl + 'deb/o_' + index + '.jpg'
+      // const src = Env.imageUrl + 'deb/deb.jpg'
       const o = {
         id: index,
         src,
@@ -127,6 +126,8 @@ function Index() {
         zIndex,
         bounds,
       }
+      console.log(o)
+      console.log(index)
 
       mapContext.addGroundOverlay(o)
       .then(res => {
@@ -138,6 +139,117 @@ function Index() {
     }
   }
 
+  /**
+  // test
+  const northeast = {
+    latitude: ne[0],
+    longitude: ne[1]
+  }
+  const southwest = {
+    latitude: sw[0],
+    longitude: sw[1]
+  }
+  const bounds = {
+    northeast,
+    southwest,
+  }
+
+  const markers1 = [
+    {
+      id: 5,
+      latitude: ne[0],
+      longitude: ne[1],
+      width: 16,
+      height: 24
+    },
+    {
+      id: 6,
+      latitude: sw[0],
+      longitude: sw[1],
+      width: 16,
+      height: 24
+    },
+  ]
+
+  mapContext.addMarkers({
+    markers: markers1,
+  }) 
+  .then(res => {
+    console.log('markers added')
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+  const src = Env.imageUrl + 'deb/o_' + 0 + '.jpg'
+  // const src = Env.imageUrl + 'deb/deb.jpg'
+  const o = {
+    id: 2,
+    src,
+    opacity,
+    zIndex,
+    bounds,
+  }
+  console.log(o)
+
+  mapContext.addGroundOverlay(o)
+  // end test
+  //
+  // test 1
+  const northeast1 = {
+    latitude: ne1[0],
+    longitude: ne1[1]
+  }
+  const southwest1 = {
+    latitude: sw1[0],
+    longitude: sw1[1]
+  }
+  const bounds1 = {
+    northeast: northeast1,
+    southwest: southwest1,
+  }
+
+  const markers2 = [
+    {
+      id: 5,
+      latitude: ne1[0],
+      longitude: ne1[1],
+      width: 16,
+      height: 24
+    },
+    {
+      id: 6,
+      latitude: sw1[0],
+      longitude: sw1[1],
+      width: 16,
+      height: 24
+    },
+  ]
+
+  mapContext.addMarkers({
+    markers: markers2,
+  }) 
+  .then(res => {
+    console.log('markers added')
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+  const src1 = Env.imageUrl + 'deb/o_' + 1 + '.jpg'
+  // const src = Env.imageUrl + 'deb/deb.jpg'
+  const o1 = {
+    id: 5,
+    src: src1,
+    opacity,
+    zIndex,
+    bounds: bounds1,
+  }
+  console.log(o1)
+
+  mapContext.addGroundOverlay(o1)
+  // end test1
+  */
 
   useEffect(() => {
   }, [])
