d7a5f76fd550c2661d8bb499152acf513ea819e1
Al Zee <z@alz.ee>
2024-06-14 13:27:53 +0800

    nth

--- src/pages/nav/index.tsx
+++ src/pages/nav/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, WebView, Map } from '@tarojs/components'
+import { View, Map } from '@tarojs/components'
 import './index.scss'
 import Taro from '@tarojs/taro'
 import { Env } from '../../env'
