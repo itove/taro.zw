404fc571e4f03b7410d38a27c9c2a4a940643ac0
Al Zee <z@alz.ee>
2024-10-20 22:10:32 +0800

    search/index searchbar

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from '@tarojs/components'
-import { Button } from "@nutui/nutui-react-taro"
+import { SearchBar } from '@nutui/nutui-react-taro'
 import './index.scss'
+import Taro from '@tarojs/taro'
+
+function search(keyword) {
+  console.log('search: ', keyword)
+  Taro.navigateTo({url: 'result?k=' + keyword})
+}
 
 function Index() {
+  const [keyword, setKeyword] = useState('')
+
   return (
-    <View className="nutui-react-demo">
-      <View className="index">
-        欢迎使用 NutUI React 开发 Taro 多端项目。
-      </View>
-      <View className="index">
-        <Button type="primary" className="btn">
-          NutUI React Button
-        </Button>
-      </View>
+    <View className="search-index">
+      <SearchBar className="" shape="round"
+        right={
+          <View className="searchbtn" onClick={() => search(keyword)}>搜索</View>
+        }
+        onBlur={(e) => {setKeyword(e)} }
+        // value={keyword}
+        onSearch={(e) => search(e) }
+      />
     </View>
   )
 }
