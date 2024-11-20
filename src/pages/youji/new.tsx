23818823a2e6ee87f5eeca8ed20f750cbe7c214d
Al Zee <z@alz.ee>
2024-11-21 02:16:15 +0800

    page youji new WIP

--- src/pages/youji/new.tsx
+++ src/pages/youji/new.tsx
@@ -11,14 +11,88 @@ import {
   Picker,
   Cell,
   Uploader,
+	DatePicker,
 } from '@nutui/nutui-react-taro'
 
 function Index() {
   const [disabled, setDisabled] = useState(false)
   const [uid, setUid] = useState(0)
-  const uploadUrl = 'https://my-json-server.typicode.com/linrufeng/demo/posts'
+  const uploadUrl = Env.apiUrl + 'media_objects'
   const [defaultFileList, setDefaultFileList] = useState([])
   const [steps, setSteps] = useState([])
+  const [imgs, setImgs] = useState([])
+  const [pickerVisible, setPickerVisible] = useState(false)
+  const [datePickerShow, setDatePickerShow] = useState(false)
+  const [pickerIndex, setPickerIndex ] = useState(0)
+  const pickerOpts = [
+    { value: 0, text: '请选择' },
+    { value: 1, text: '1月' },
+      { value: 2, text: '2月' },
+      { value: 3, text: '3月' },
+      { value: 4, text: '4月' },
+      { value: 5, text: '5月' },
+      { value: 6, text: '6月' },
+      { value: 7, text: '7月' },
+      { value: 8, text: '8月' },
+      { value: 9, text: '9月' },
+      { value: 10, text: '10月' },
+      { value: 12, text: '11月' },
+      { value: 12, text: '12月' },
+  ]
+
+  const Step = ({step, index}) => {
+    return (
+      <View className="step d-flex align-items-center my-4" key={index}>
+        <View className="date" onClick={() => setDatePickerShow(!datePickerShow) }>选择日期时间</View>
+        <TextArea className="body" autoSize placeholder="输入行程内容..." value={steps[index].body}
+          onChange={(e) => stepBodyChange(e, index)}
+        />
+        <img width="16px" height="16px" src={Env.iconUrl + 'x-circle-fill-blue.svg'} 
+          onClick={(e) => rmStep(index)}
+        />
+      </View>
+    )
+  }
+
+  const stepBodyChange = (e, index) => {
+    console.log('step body changed ', e)
+    setSteps(steps.map((step, i) => i === index ? {...step, body: e} : step))
+  }
+
+  const addStep = (e) => {
+    console.log('add step ')
+    // console.log(e)
+    setSteps([...steps, {date: new Date(), body: ''}])
+  }
+
+  const rmStep = (i) => {
+    console.log('rm step ', i)
+    setSteps(steps.filter((_, index) => index !== i))
+  }
+
+  const dateChange = (options, value, index) => {
+    console.log('date change')
+    // console.log(options)
+    // console.log(value)
+    // console.log(index)
+  }
+
+  const dateConfirm = (options, value) => {
+    console.log(options)
+    console.log(value)
+  }
+
+  const pickerChange = (options, value) => {
+    console.log('picker change')
+    // console.log(options)
+    // console.log(value)
+  }
+
+  const pickerConfirm = (options, value) => {
+    console.log(options)
+    console.log(value)
+    setPickerIndex(value[0])
+  }
 
   const formSubmit = (data) => {
     setDisabled(true)
@@ -57,6 +131,11 @@ function Index() {
   const onFinishFailed = () => {
     console.log('verify failed')
   }
+
+  const uploadSuccess = (e) => {
+    console.log(e)
+  }
+
   return (
     <View className="youji-new">
       <Form 
@@ -74,7 +153,7 @@ function Index() {
             { required: true, message: '请上传图片' },
           ]}
         >
-          <Uploader defaultValue={defaultFileList} url={uploadUrl} multiple maxCount="9" />
+          <Uploader name="upload" defaultValue={defaultFileList} url={uploadUrl} multiple maxCount="9" onSuccess={(e) => uploadSuccess(e) } />
         </Form.Item>
 
         <Form.Item
@@ -98,7 +177,8 @@ function Index() {
         >
           <Cell
 					align="center"
-          title={ <Picker title="请选择" /> }
+          title={pickerOpts[pickerIndex].text}
+          onClick={() => setPickerVisible(!pickerVisible)}
           extra={<img width="16px" height="16px" src={Env.iconUrl + 'arrow-right.png'} className="icon" />}
           />
         </Form.Item>
@@ -145,13 +225,11 @@ function Index() {
           <Input placeholder="请输入总行程概览" type="text" />
         </Form.Item>
 
-        <View>
-          <Picker title="选择日期" />
-          <Picker title="选择时间" />
-          <Input placeholder="输入行程内容..." />
+        <View className="steps">
+          {steps.map((step, index) => <Step step={step} index={index} />)}
         </View>
 
-        <View className="p-1 add d-flex align-items-center">
+        <View className="p-1 add d-flex align-items-center" onClick={(e) => addStep(e)} >
           <img width="16px" height="16px" src={Env.iconUrl + 'plus-circle-blue.svg'} className="me-8" />
           添加子行程
         </View>
@@ -176,6 +254,26 @@ function Index() {
           <Button disabled={disabled} formType="submit" className="btn-primary btn2 m-0">发布</Button>
         </View>
       </Form>
+
+      <Picker
+        visible={pickerVisible}
+        options={pickerOpts}
+        onClose={() => setPickerVisible(false)}
+        onConfirm={(options, value) => pickerConfirm(options, value)}
+        onChange={(options, value) => pickerChange(options, value)}
+        title="请选择出行月份"
+      /> 
+      <DatePicker
+        title="选择子行程日期时间"
+        startDate={new Date()}
+        // endDate={endDate}
+        defaultValue={new Date()}
+        visible={datePickerShow}
+        type="datetime"
+        onClose={() => setDatePickerShow(false)}
+        onChange={(options, value, index) => dateChange(options, value, index)}
+        onConfirm={(options, value) => dateConfirm(options, value)}
+      />
     </View>
   )
 }
