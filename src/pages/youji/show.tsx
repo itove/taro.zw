7ab2df4df9bea9c906ace1d252007b17f8f1bbba
Al Zee <z@alz.ee>
2024-11-21 17:08:15 +0800

    page youji show WIP

--- src/pages/youji/show.tsx
+++ src/pages/youji/show.tsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { View, Button, Image } from '@tarojs/components'
-import './index.scss'
+import { View, Image, Input, Button } from '@tarojs/components'
+import './show.scss'
 import Taro from '@tarojs/taro'
 import { Env } from '../../env'
+import { Tabs, Rate, ImagePreview } from '@nutui/nutui-react-taro'
+import { fmtSeconds } from '../../utils/fmtSeconds'
 import { fmtDate } from '../../utils/fmtDate'
-import { Tabs, Empty } from '@nutui/nutui-react-taro'
+
+Taro.options.html.transformElement = (el) => {
+  if (el.nodeName === 'image') {
+    el.setAttribute('mode', 'widthFix')
+    el.setAttribute('src', Env.baseUrl + el.getAttribute('src'))
+  }
+  return el
+}
 
 function gotoNode(nid, type) {
   console.log('go to node ', nid);
@@ -17,142 +26,62 @@ function gotoNode(nid, type) {
 }
 
 function Index() {
-  const [nodes, setNodes] = useState([])
-  const [nodes1, setNodes1] = useState([])
+  const [node, setNode] = useState({rates: {rate: 0, users: []}, tags: []})
   const [uid, setUid] = useState(0)
-  const [user, setUser] = useState({})
-  const [tab1value, setTab1value] = useState<string | number>('0')
+  const [body, setBody] = useState('')
+  const [comment, setComment] = useState('')
+  const [comments, setComments] = useState([])
+  const [logged, setLogged] = useState(false)
+  const [commentCount, setCommentCount] = useState(0)
+  const [rateCount, setRateCount] = useState(0)
+  const [liked, setLiked] = useState(false)
+  const [likeCount, setLikeCount] = useState(0)
+
+  const instance = Taro.getCurrentInstance();
+  const nid = instance.router.params.nid
 
   useEffect(() => {
-    Taro.getStorage({
-      key: Env.storageKey
+    Taro.request({
+      url: Env.apiUrl + 'youji/' + nid
     })
     .then(res => {
-      let u = res.data
-      setUser(u)
-      setUid(u.id)
-      Taro.request({
-        url: Env.apiUrl + 'youji?uid=' + res.data.id
-      })
-      .then(res => {
-        console.log(res)
-        setNodes(res.data[0])
-        setNodes1(res.data[1])
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      // Taro.redirectTo({url: '/pages/me/login'})
+      const n = res.data
+      setNode(n)
+      if (n.body) {
+        setBody(n.body.replace(/&nbsp;/g, '<br/>'))
+      }
     })
   }, [])
 
-  function delNode(nid, uid) {
-    console.log('delete node ', nid);
-    console.log('uid ', uid);
-      Taro.showModal({
-        title: '提示',
-        content: '确定删除？',
-        success: function (res) {
-          if (res.confirm) {
-            Taro.request({
-              method: 'DELETE',
-              data: { uid: uid },
-              url: Env.apiUrl + 'youji/' + nid
-            }).then((res) =>{
-              if (res.statusCode === 200) {
-                Taro.request({
-                  url: Env.apiUrl + 'youji?uid=' + uid
-                })
-                .then(res => {
-                  console.log(res)
-                  setNodes(res.data[0])
-                  setNodes1(res.data[1])
-                })
-                Taro.showToast({
-                  title: '草稿已取消',
-                  icon: 'success',
-                  duration: 2000,
-                  success: () => {
-                    // setTimeout(
-                    //   () => {
-                    //     Taro.reLaunch({url: '/pages/order/index'})
-                    //   }, 500
-                    // )
-                  }
-                })
-              }
-            })
-          } else if (res.cancel) {
-          }
-        }
-      })
-  }
+  return (
+    <View className="youji-show">
+      <View className="hero">
+        <Image className="w-100 rounded" src={Env.imageUrl + node.image} mode="heightFix" />
+      </View>
+      <View className="title mb-10">
+        {node.title}
+      </View>
 
-  function GridItem({node, index, type, uid}) {
-    return (
-      <View key={index} className="grid-item rounded overflow-hidden">
-        <Image onClick={() => gotoNode(node.id, type)} className="w-100 img" src={Env.imageUrl + node.image} mode="aspectFill" />
-        <View className="text">
-          <View className="ellipsis mb-8">{node.title}</View>
-          <View className="more">
-            <View>{fmtDate(new Date(node.createdAt), 4)}</View>
-            { type === 0 &&
-            <View className="fav">
-              <img className="icon me-5" height="16px" width="16px" src={Env.iconUrl + 'heart-pink-fill.svg'} />
-              <View className="count">{node.favs.length}</View>
-            </View>
-            ||
-            <View className="fav">
-              <img className="icon me-5" height="16px" width="16px" src={Env.iconUrl + 'trash.svg'} onClick={() => delNode(node.id, uid) } />
-            </View>
-            }
+      <View className="plan mb-10">
+        <View>
+          <View>出发时间</View>
+          <View>5月</View>
+        </View>
+      </View>
+
+      <View className="steps mb-10">
+        <View>行程单</View>
+        <View>
+          <View>
           </View>
         </View>
       </View>
-    )
-  }
 
-  return (
-    <View className="youji-index p-1">
-      <Tabs
-        value={tab1value}
-        autoHeight={true}
-        align="left"
-        activeType="smile"
-        onChange={(value) => {
-          setTab1value(value)
-        }}
-        className=""
-      >
-        <Tabs.TabPane title="我发布的">
-          { nodes.length > 0 &&
-          <View className="grid">
-            {nodes.map((node, index) => <GridItem node={node} index={index} type={0} uid={uid} />)}
-          </View>
-          ||
-          <View>
-            <Empty status="empty" description="抱歉，未找到相关内容哦~"
-            image={<img src={Env.imageUrl+ 'empty.png'} />}
-            imageSize={120}
-            />
-          </View>
-          }
-        </Tabs.TabPane>
-        <Tabs.TabPane title="我的草稿">
-          { nodes1.length > 0 &&
-          <View className="grid">
-            {nodes1.map((node, index) => <GridItem node={node} index={index} type={1} uid={uid} />)}
-          </View>
-          ||
-          <View>
-            <Empty status="empty" description="抱歉，未找到相关内容哦~"
-            image={<img src={Env.imageUrl+ 'empty.png'} />}
-            imageSize={120}
-            />
-          </View>
-          }
-        </Tabs.TabPane>
-      </Tabs>
+      <View>详情</View>
+      <View className="images mb-10">
+      </View>
+
+      <View dangerouslySetInnerHTML={{__html: body}} className='body py-16'></View>
     </View>
   )
 }
